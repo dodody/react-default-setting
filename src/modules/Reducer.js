7ef428987c1a4e5282759f3c6e2548c4604bbf7e
dodody <dody@bigpearl.io>
2020-07-29 20:39:10 +0900

    chore: 📦redux 관련 패키지 추가 및 세팅

---
+++ src/modules/Reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { penderReducer } from 'redux-pender';
+
+import user from './user';
+
+export default combineReducers({
+  user, // 채널 디테일 페이지
+
+  // 리덕스 미들웨어
+  pender: penderReducer
+});
